febac6a2b2fa60a4e92aed4d6469b6b64bd96b89
yueyuea <1327944851@qq.com>
2022-11-03 21:05:08 +0800

    first submit

---
+++ White_Board/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for White_Board project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'White_Board.settings')
+
+application = get_asgi_application()
