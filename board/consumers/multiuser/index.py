2030af97499a54d2f398dbfdd0a37a874de4d24b
yueyuea <1327944851@qq.com>
2022-11-11 15:38:59 +0800

    websocket received

--- board/consumers/multiuser/index.py
+++ board/consumers/multiuser/index.py
@@ -1,33 +1,33 @@
 # chat/consumers.py
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.cache import cache
 
-class MultiUser(WebsocketConsumer):
-    def connect(self):
+class MultiUser(AsyncWebsocketConsumer):
+    async def connect(self):
         self.roomid = self.scope['url_route']['kwargs']['roomid']
         self.mode = self.scope['url_route']['kwargs']['mode']
         self.room_group_name = 'board_%s' % self.roomid
         print("roomid:" + self.roomid + " mode:" + self.mode)
         # self.room_group_name = 'board_11'
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
         print("成功连接")
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
-    def receive(self, text_data:str) -> None:
+    async def receive(self, text_data:str) -> None:
         print(text_data)
         text_data_json = json.loads(text_data)
         print(text_data_json)
@@ -35,7 +35,7 @@ class MultiUser(WebsocketConsumer):
         # json_str = json.dumps(python2json)
         # message=JSON.stringify(text_data_json)
         print("----")
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {
                 'type': 'chat.message',  # 必须在MsgConsumer类中定义chat_message
                 'message': text_data_json
@@ -43,7 +43,7 @@ class MultiUser(WebsocketConsumer):
 
 
     # Receive message from room group
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
         # Send message to WebSocket
