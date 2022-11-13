974c910e59f9e7b4c56db2c042070b6d373147ae
yueyuea <1327944851@qq.com>
2022-11-13 12:05:48 +0800

    websocket server

--- board/consumers/multiuser/index.py
+++ board/consumers/multiuser/index.py
@@ -1,9 +1,10 @@
 # chat/consumers.py
 import json
-from asgiref.sync import async_to_sync
+# from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.core.cache import cache
 import random
+
 class MultiUser(AsyncWebsocketConsumer):
     async def connect(self):
         self.roomid = self.scope['url_route']['kwargs']['roomid']
@@ -14,10 +15,8 @@ class MultiUser(AsyncWebsocketConsumer):
             while cache.has_key(roomid):
                 roomid = random.randint(2, 100)
             cache.set(roomid, [], 3600)
-            # roomid = tmp
         self.room_group_name = 'board_%s' % roomid
-        # print("roomid:" + roomid + " mode:" + self.mode)
-        # self.room_group_name = 'board_11'
+        print("roomid:" + roomid + " mode:" + self.mode)
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -25,7 +24,6 @@ class MultiUser(AsyncWebsocketConsumer):
         )
         print("成功连接")
         await self.accept()
-        
         await self.send(
             text_data=json.dumps({
             'roomid': roomid
@@ -38,27 +36,60 @@ class MultiUser(AsyncWebsocketConsumer):
             self.channel_name
         )
 
+    async def paint_regular_graphics(self, data):
+        print("发送规则图形")
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':"send_message",
+                'event':"regular_graphics",
+                'style':data['style'],
+                'startX':data['startX'],
+                'startY':data['startY'],
+                'endX':data['endX'],
+                'endY':data['endY'],
+            }
+        )
+
+    async def paint_bezier_curve(self, data):
+        print("发送贝塞尔曲线")
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':"send_message",
+                'event':"bezier_curve",
+                'style':data['style'],
+                'start':data['start'],
+                'control':data['control'],
+                'end':data['end'],
+            }
+        )
+
     # Receive message from WebSocket
     async def receive(self, text_data:str) -> None:
-        print(text_data)
-        text_data_json = json.loads(text_data)
-        print(text_data_json)
-        # message = text_data_json['data']['color']
-        message = text_data_json['opt']
-        # json_str = json.dumps(python2json)
-        # message=JSON.stringify(text_data_json)
-        print("----")
-        await self.channel_layer.group_send(
-            self.room_group_name, {
-                'type': 'chat.message',  # 必须在MsgConsumer类中定义chat_message
-                'message': text_data_json
-            })
+        data = json.loads(text_data)
+        event = data['event']
+        print(data)
+        if event == 'regular_graphics':
+            await self.paint_regular_graphics(data)
+        elif event == 'bezier_curve':
+            await self.paint_bezier_curve(data)
 
+        await self.channel_layer.group_send(
+        self.room_group_name, {
+            'type': 'chat.message',  # 必须在MsgConsumer类中定义chat_message
+            'message': data
+        })
+
+    # Receive message from room group
+    async def send_message(self, data):
+
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps(data))
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-
         # Send message to WebSocket
         await self.send(
             text_data=json.dumps({
