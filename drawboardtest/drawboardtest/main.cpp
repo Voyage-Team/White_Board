c0cbc01f6c8de34d50677bfe250f10106b6ff460
UEOAdora <748239754@qq.com>
2022-11-13 17:14:41 +0800

    clear

--- drawboardtest/drawboardtest/main.cpp
+++ drawboardtest/drawboardtest/main.cpp
@@ -9,5 +9,18 @@ int main(int argc, char *argv[])
     //MainWindow w;
     //w.show();
     Kernel kernel;
+//    QJsonObject qobj;
+//    qobj.insert("obj:","add");
+//    QJsonObject total;
+//    total.insert("figure:",QJsonValue(qobj));
+//    total.insert("otp","add");
+//    qDebug() <<total;
+//    QString type = total.value("obj:").toString();
+//    if(type == "add")
+//    {
+//        qDebug("add");
+//    } else {
+//        qDebug("no");
+//    }
     return a.exec();
 }
